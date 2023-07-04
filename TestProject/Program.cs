235f9557ff42d708bdade08d7b05bec8460d8b4a
Soller <esolergonzalez@gmail.com>
2023-07-04 01:52:46 -0600

    Hero vs Monster

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,85 +1,46 @@
 ﻿using System;
 
-// for (int i = 10; i >= 0; i--)
-// {
-//     Console.WriteLine(i);
-// }
-
-
-// for (int i = 0; i < 10; i += 3)
-// {
-//     Console.WriteLine(i);
-// }
-
-// Explicación de break en un for
-// for (int i = 0; i < 10; i++)
-// {
-//     Console.WriteLine(i);
-//     if (i == 7) break;
-// }
-
-// string[] names = { "Adam", "Barry", "Charlie", "Michael" };
-
-// for (int i = 0; i < names.Length; i++)
-//   if (names[i] == "Charlie")
-//     names[i] = "Sammy";
-
-// foreach (var name in names)
-//   Console.WriteLine(name);
-
-// desafío fizzbuzz
-// for (int i = 1; i <= 100; i++)
-// {
-//     if (i % 3 == 0 && i % 5 == 0)
-//     {
-//         Console.WriteLine($"{i} FizzBuzz");
-//     } else if (i % 3 == 0)
-//     {
-//         Console.WriteLine($"{i} Fizz");
-//     } else if (i % 5 == 0)
-//     {
-//         Console.WriteLine($"{i} Buzz");
-//     } else
-//     {
-//         Console.WriteLine(i);
-//     }
-// }
-// Console.WriteLine("Presione cualquier tecla para salir");
-// Console.ReadLine();
-
-// Console.WriteLine("Presione cualquier tecla para salir");
-// Console.ReadLine();
-
-// Quiero un programa que haga el desafio de fizz buzz
-//Conviertelo a una funcion
-
-internal class Program
+namespace TestProject
 {
-  private static void Main(string[] args)
+  class Program
   {
-    Func<int, string> fizzBuzz = (i) =>
-{
-  if (i % 3 == 0 && i % 5 == 0)
-  {
-    return $"{i} FizzBuzz";
-  }
-  else if (i % 3 == 0)
-  {
-    return $"{i} Fizz";
-  }
-  else if (i % 5 == 0)
-  {
-    return $"{i} Buzz";
-  }
-  else
-  {
-    return $"{i}";
-  }
-};
-
-    for (int i = 1; i <= 100; i++)
+    static void Main(string[] args)
     {
-      Console.WriteLine(fizzBuzz(i));
+      int heroHealth = 10;
+      int monsterHealth = 10;
+      Random random = new Random();
+
+      Console.WriteLine("The fight is starting!");
+
+      do
+      {
+        //Ataque del heroe
+        int heroAttack = random.Next(1, 11);
+        monsterHealth -= heroAttack;
+
+        Console.WriteLine($"The hero attacks and deals {heroAttack} damage to the monster!");
+        Console.WriteLine($"The monster has {monsterHealth} health left.");
+
+        if (monsterHealth <= 0)
+          break;
+
+        //Ataque del monstruo
+        int monsterAttack = random.Next(1, 11);
+        heroHealth -= monsterAttack;
+        
+        Console.WriteLine($"The monster attacks and deals {monsterAttack} damage to the hero!");
+        Console.WriteLine($"The hero has {heroHealth} health left.");
+
+      } while (heroHealth > 0 && monsterHealth > 0);
+
+      Console.WriteLine("The fight is over!");
+
+      if (heroHealth > 0)
+        Console.WriteLine("The hero won!");
+      else if (monsterHealth > 0)
+        Console.WriteLine("The monster won!");
+      else
+        Console.WriteLine("Both dont have health, its a draw!");
     }
   }
 }
